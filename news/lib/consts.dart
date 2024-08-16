d7967efd2f0c8de571d7e9fdfa1a7a3b756b60eb
AsimHayat <asimhayat125@gmail.com>
2024-08-16 07:32:47 +0500

    News fetching from Api

---
+++ news/lib/consts.dart
@@ -0,0 +1,4 @@
+const String NEWS_API_KEY = "e46d9004804e40d6aea91e3af920d88d";
+
+const String PLACEHOLDER_IMAGE_LINK =
+    "https://developers.elementor.com/docs/assets/img/elementor-placeholder-image.png";
